9a1364162a21031bae60feb7dd8e1434d618870e
IsSabuhi <sabuhi.israfilov2001@gmail.com>
2022-04-07 16:49:40 +0700

    change loaddataset

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -24,19 +24,19 @@
           <v-btn color="success" @click="predict()">Распознать</v-btn>
         </div>
         <div class="mr-4 my-2">
-          <v-btn color="primary" >Инициализировать веса</v-btn>
+          <v-btn color="primary" @click="calculateWeights()">Инициализировать веса</v-btn>
         </div>
         <div>
           <v-btn color="primary" @click="$refs.inputUpload.click()">Загрузить датасет</v-btn> 
-          <input multiple accept=".png" v-show="false" ref="inputUpload" type="file" id="load" @change="(e) => loadDataset(e)">
+          <input multiple accept=".png" v-show="false" ref="inputUpload" type="file" id="dataset" @change="(e) => loadDataset(e)">
         </div>
         <div class="mt-2">
           <v-btn color="primary" @click="save(weightMatrix)">Сохранить веса</v-btn>
         </div>
-        <div class="mt-2">
+        <!-- <div class="mt-2">
           <v-btn color="primary" @click="$refs.inputUpload.click()">Загрузить веса</v-btn> 
-          <input v-show="false" ref="inputUpload" type="file" id="load" @change="(e) => processFile(e)">
-        </div>        
+          <input multiple v-show="false" ref="inputUpload" type="file" id="load" @change="(e) => processFile(e)">
+        </div>         -->
       </v-sheet>
 
     </div>
@@ -46,7 +46,6 @@
 <script>
 import Vue from 'vue';
 import VueDrawingCanvas from 'vue-drawing-canvas';
-import lodash from 'lodash'
 export default Vue.extend({
   name: 'HomeComponent',
   components: {
@@ -65,13 +64,31 @@ export default Vue.extend({
     imagesCount: 10,
   }),
   methods: {
+    calculateWeights(){
+      let weightMax = 0.3
+      let weightMin = -weightMax
+      this.weightMatrix = this.initWeights(this.width, this.height, weightMax, weightMin)
+    },
+    initWeights(width, height, max, min) {
+      let weights = []
+      for (let i = 0; i < width * height; i++) {
+        weights.push(Number((Math.random() * (max - min) + min).toFixed(2)))        
+      }
+      console.log('Веса инициализированы')
+      console.log(weights)
+      return weights
+      
+    },
+    shuffle(array) {
+      array.sort(() => Math.random() - 0.5);
+    },
     async loadDataset(e) {
       let time = performance.now();
-      let _ = require(lodash);
+      let _ = require('lodash');
       let files = e.target.files;
       files = Object.values(files);
-
-      files = canvas.shuffle(files);
+      console.log(files)
+      files = this.shuffle(files);
 
       let vectorsAndAnswer = [];
 
@@ -143,6 +160,121 @@ export default Vue.extend({
         
     },
 
+    constructor(imagesRes, neuronNumber, thresholdError, speedLearn) {
+        for (let i = 0; i < neuronNumber; i++) {
+          let weightNeuron = [];
+          for (let j = 0; j < imagesRes * imagesRes; j++) {
+            let randomNumber = Math.random() * (0.31 + 0.31) - 0.31;
+            weightNeuron.push(randomNumber);
+          }
+          this.weights.push(weightNeuron);
+        }
+
+        this.thresholdError = thresholdError;
+        this.speedLearn = speedLearn;
+      },
+
+    // вычисление сигмоиды
+    $sigmoid(sum) {
+      return 1 / (1 + Math.exp(-sum));
+    },
+
+    // производная сигмоиды
+    $sigmoid_derivative(sigmoid) {
+      return sigmoid * (1 - sigmoid);
+    },
+
+    Predict(vector) {
+    let neuronSums = [];
+
+    for (let i = 0, neuronLen = this.weights.length; i < neuronLen; i++) {
+      let sum = 0;
+      for (
+        let j = 0, weightsLen = this.weights[i].length;
+        j < weightsLen;
+        j++
+      ) {
+        sum += vector[j] * this.weights[i][j];
+      }
+
+      neuronSums.push(this.$sigmoid(sum));
+    }
+
+    return neuronSums;
+  },
+
+  Train(vector, correctAnswers) {
+    let answer = this.Predict(vector);
+
+    for (let i = 0, neuronLen = this.weights.length; i < neuronLen; i++) {
+      let weightsDelta = correctAnswers[i] - answer[i];
+
+      for (
+        let j = 0, weightsLen = this.weights[i].length;
+        j < weightsLen;
+        j++
+      ) {
+        if (vector[j] === 1) {
+          this.weights[i][j] += this.speedLearn * weightsDelta * vector[j];
+        }
+      }
+    }
+  },
+
+  TrainDataset(vectorsAndAnswers) {
+    for (let i = 0, arrLen = vectorsAndAnswers.length; i < arrLen; i++) {
+      this.Train(vectorsAndAnswers[i].vector, vectorsAndAnswers[i].answer);
+    }
+
+    let correctCount = 0;
+
+    for (let i = 0, arrLen = vectorsAndAnswers.length; i < arrLen; i++) {
+      let neuronOutput = this.Predict(vectorsAndAnswers[i].vector);
+
+      let sumError = 0;
+      for (let j = 0, outputLen = neuronOutput.length; j < outputLen; j++) {
+        const error = Math.pow(
+          neuronOutput[j] - vectorsAndAnswers[i].answer[j],
+          2
+        );
+        sumError += error;
+      }
+
+      if (sumError < this.thresholdError) correctCount++;
+    }
+
+    console.log(vectorsAndAnswers.length - correctCount + ' ошибок');
+
+    this.errors += vectorsAndAnswers.length - correctCount;
+
+    let correctPercent = (correctCount / vectorsAndAnswers.length) * 100;
+
+    return correctPercent;
+  },
+
+    saveWeights() {
+    const a = document.createElement('a');
+
+    let data = JSON.stringify(this.weights);
+
+    const file = new Blob([data], { type: 'application/json' });
+
+    a.href = URL.createObjectURL(file);
+    a.download = 'weights.json';
+    a.click();
+
+    URL.revokeObjectURL(a.href);
+  },
+    readFileAsync(file) {
+      return new Promise((resolve, reject) => {
+        let reader = new FileReader();
+        reader.onload = () => {
+          resolve(reader.result);
+        };
+        reader.onerror = reject;
+        reader.readAsText(file);
+      })
+    },
     async processFile(e) {
         try {
           let file = e.target.files[0];
@@ -154,10 +286,12 @@ export default Vue.extend({
         } catch(err) {
           console.log(err);
         }
-    }
+    },
 
-
-}
+}, 
+mounted(){
+    this.calculateWeights()
+  }
 
 });
 </script>
